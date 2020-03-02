36a8236bd50f7780c927e87418b8b4f25025c91b
dmsteger <thedansteger@gmail.com>
2020-03-01 22:32:01 -0800

    Working on bottom of page results

--- webroot/api/index.php
+++ webroot/api/index.php
@@ -68,6 +68,10 @@
 
         break;
 
+        case "whatever":
+
+        break;
+
         default:
             echo "Somehow a search type was not selected.";
     }
@@ -77,41 +81,15 @@
     if ($curl_response === false) {
         $info = curl_getinfo($curl);
         curl_close($curl);
-        die('error occured during curl exec. Additioanl info: ' . var_export($info));
+        die('error occured during curl exec. Additional info: ' . var_export($info));
     }
     curl_close($curl);
     $decoded = json_decode($curl_response);
-
-    // print_r($decoded);
-
-    /*
-    if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-        die('error occured: ' . $decoded->response->errormessage);
-    }
-    // echo 'response ok!'."\n";
-    // echo $curl_response."\n";
-    // echo gettype($decoded)."\n";
-    // echo $decoded[0]->name;
-
-    */
-
-    // function sortByPopulation($a, $b)
-    // {
-    //     $a = $a['population'];
-    //     $b = $b['population'];
-
-    //     if ($a == $b) return 0;
-    //     return ($a < $b) ? -1 : 1;
-    // }
 
     //sort decoded array by population
     usort($decoded, function($a, $b) { //Sort the array using a user defined function
         return $a->population > $b->population ? -1 : 1; //Compare the populations
     }); 
-
-    // usort($decoded, sortByPopulation);
-
-    // $results = print_r($decoded, true);
 
     header('Content-type: application/json');
     echo json_encode($decoded);
--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,14 +1,17 @@
 const init = () => {
 
     let countryButton = document.querySelector("#getCountryByName");
-    countryButton.addEventListener("click", callNameApi);
+    countryButton.addEventListener("click", callApi);
 
 }
 
-const callNameApi = () => {
+const callApi = () => {
+
+    var countryNames = [];
+    var regionNames = [];
+    var subregionNames = [];
 
     let oldNode = document.getElementById("countryContent");
-
     document.body.removeChild(oldNode);
 
     let node = document.createElement("div");
@@ -20,17 +23,18 @@ const callNameApi = () => {
 
     let search = document.getElementById("search").value;
     let searchType = document.getElementById("searchType").value;
+    console.log(search);
     console.log(searchType);
 
     if (search.length == 0) {
-        let warning = document.createElement("p");
-        warning.innerHTML = 'You must enter a value';
-        // document.body.appendChild(warning);
+        let warning = document.createElement("h2");
+        warning.innerHTML = 'Please Enter a Search Term';
         contentDiv.appendChild(warning);
         inputCheck = false;
     }
 
     if (inputCheck) {
+
         let xhr = new XMLHttpRequest();
         let url = "api/index.php?search=" + search + "&searchType=" + searchType;
 
@@ -38,34 +42,221 @@ const callNameApi = () => {
 
         xhr.onreadystatechange = () => {
             if(xhr.readyState == 4) {
+
+        
                 let response = JSON.parse(xhr.responseText);
 
+                let responseSize = response.length;
 
-    //full name, alpha code 2, alpha code 3, flag image, region, subregion, population, and a list
-    // of its languages
-                for (let country of response) {
-                    console.log(country.name);
-                    console.log(country.name);
-                    console.log(country.name);
-                    console.log(country.name);
-                    console.log(country.name);
-                    console.log(country.name);
-                    console.log(country.name);
-                    console.log(country.name);
+                console.log(responseSize);
+                console.log("Response type: " + typeof(response));
+                console.log("Response size:" + typeof(responseSize));
+                console.log(response.status);
+                console.log(response.name);
+
+                if ((response.status != 404) && (searchType != "code")) {
+
+                        let resultHeader = document.createElement("h2");
+                        resultHeader.innerHTML = "Results (in order of population):";
+                        contentDiv.appendChild(resultHeader);    
+
+        //full name, alpha code 2, alpha code 3, flag image, region, subregion, population, and a list
+        // of its languages
+                    for (let country of response) {
+
+                        console.log(country.name);
+                        console.log(country.alpha2Code);
+                        console.log(country.alpha3Code);
+                        console.log(country.region);
+                        console.log(country.subregion);
+                        console.log(country.population);
+                        
+                        let countryName = country.name;
+                        let alpha2Code = country.alpha2Code;
+                        let alpha3Code = country.alpha3Code;
+                        let region = country.region;
+                        let subregion = country.subregion;
+                        let population = country.population;
+                        let flagUrl = country.flag;
+
+                        countryNames.push(countryName);
+                        regionNames.push(region);
+                        subregionNames.push(subregion);
+
+                        console.log(countryNames);
+
+                        let countryDiv = document.createElement("div");
+                        countryDiv.setAttribute("id", alpha3Code);
+                        contentDiv.appendChild(countryDiv);
+
+                        let countryHeading = document.createElement("h3");
+                        countryHeading.setAttribute("class", "countryHeading");
+                        countryHeading.innerHTML = countryName;
+
+                        let flagImage = document.createElement("img");
+                        flagImage.setAttribute("src", flagUrl);
+                        flagImage.setAttribute("alt", "Flag of " + countryName);
+                        flagImage.setAttribute("height", 100);
+                        flagImage.setAttribute("width", 150);
+
+                        let countryInformation = document.createElement("ul");
+                        countryInformation.setAttribute("id", alpha2Code);
+
+                        let alpha2Line = document.createElement("li");
+                        alpha2Line.innerHTML = "Alpha 2 Code: " + alpha2Code;
+
+                        let alpha3Line = document.createElement("li");
+                        alpha3Line.innerHTML = "Alpha 3 Code: " + alpha3Code;
+
+                        let populationLine = document.createElement("li");
+                        populationLine.innerHTML = "Population: " + population;
+
+                        let regionLine = document.createElement("li");
+                        regionLine.innerHTML = "Region: " + region;
+
+                        let subregionLine = document.createElement("li");
+                        subregionLine.innerHTML = "Subregion: " + subregion;
+
+                        let languagesLine = document.createElement("li");
+                        languagesLine.innerHTML = "Languages Spoken: ";
+
+                        let languageList = document.createElement("ul");
+                        languageList.setAttribute("id", alpha2Code + "LanguageList");
+                        languageList.setAttribute("class", "languageList");
+
+                        countryInformation.appendChild(populationLine);
+                        countryInformation.appendChild(alpha2Line);
+                        countryInformation.appendChild(alpha3Line);
+                        countryInformation.appendChild(regionLine);
+                        countryInformation.appendChild(subregionLine);
+                        countryInformation.appendChild(languagesLine);
+                        countryInformation.appendChild(languageList);
+
+                        for (let language of country.languages) {
+                            console.log(language.name);
+                            let languageItem = document.createElement("li");
+                            languageItem.innerHTML = language.name;
+                            languageList.appendChild(languageItem);
+                        }
+                        
+                        countryDiv.appendChild(countryHeading);
+                        countryDiv.appendChild(countryInformation);
+
+                        let flagHeader = document.createElement("h4");
+                        flagHeader.innerHTML = "Flag of " + countryName + ":";
+
+                        countryDiv.appendChild(flagHeader);
+
+                        countryDiv.appendChild(flagImage);
+
+                    }
+
+                } else if (response.status != 404 && searchType == "code") {
+
+                    let countryName = response.name;
+                    let alpha2Code = response.alpha2Code;
+                    let alpha3Code = response.alpha3Code;
+                    let region = response.region;
+                    let subregion = response.subregion;
+                    let population = response.population;
+                    let flagUrl = response.flag;
+
+                    for (let language of response.languages) {
+                        console.log(language.name);
+                    }
+
+                    let countryDiv = document.createElement("div");
+                    countryDiv.setAttribute("id", alpha3Code);
+                    contentDiv.appendChild(countryDiv);
+
+                    let countryHeading = document.createElement("h3");
+                    countryHeading.setAttribute("class", "countryHeading");
+                    countryHeading.innerHTML = countryName;
+
+                    let flagImage = document.createElement("img");
+                    flagImage.setAttribute("src", flagUrl);
+                    flagImage.setAttribute("alt", "Flag of " + countryName);
+                    flagImage.setAttribute("height", 100);
+                    flagImage.setAttribute("width", 150);
+
+                    let countryInformation = document.createElement("ul");
+                    countryInformation.setAttribute("id", alpha2Code);
+
+                    let alpha2Line = document.createElement("li");
+                    alpha2Line.innerHTML = "Alpha 2 Code: " + alpha2Code;
+
+                    let alpha3Line = document.createElement("li");
+                    alpha3Line.innerHTML = "Alpha 3 Code: " + alpha3Code;
+
+                    let populationLine = document.createElement("li");
+                    populationLine.innerHTML = "Population: " + population;
+
+                    let regionLine = document.createElement("li");
+                    regionLine.innerHTML = "Region: " + region;
+
+                    let subregionLine = document.createElement("li");
+                    subregionLine.innerHTML = "Subregion: " + subregion;
+
+                    let languagesLine = document.createElement("li");
+                    languagesLine.innerHTML = "Languages Spoken: ";
+
+                    let languageList = document.createElement("ul");
+                    languageList.setAttribute("id", alpha2Code + "LanguageList");
+                    languageList.setAttribute("class", "languageList");
+
+
+
+                    countryInformation.appendChild(populationLine);
+                    countryInformation.appendChild(alpha2Line);
+                    countryInformation.appendChild(alpha3Line);
+                    countryInformation.appendChild(regionLine);
+                    countryInformation.appendChild(subregionLine);
+                    countryInformation.appendChild(languagesLine);
+                    countryInformation.appendChild(languageList);
+
+                    for (let language of response.languages) {
+                        console.log(language.name);
+                        let languageItem = document.createElement("li");
+                        languageItem.innerHTML = language.name;
+                        languageList.appendChild(languageItem);
+                    }
+
+                
+                    countryDiv.appendChild(countryHeading);
+                    countryDiv.appendChild(countryInformation);
+
+                    let flagHeader = document.createElement("h4");
+                    flagHeader.innerHTML = "Flag of " + countryName + ":";
+
+                    countryDiv.appendChild(flagHeader);
+
+                    countryDiv.appendChild(flagImage);
+
+
+
+                    console.log("looking at code");
+                } else {
+                    let warning = document.createElement("h2");
+                    warning.innerHTML = 'No results';
+                    contentDiv.appendChild(warning);            
                 }
-
-                let paragraph = document.createElement("p");
-                paragraph.innerHTML = response;
-                // document.body.appendChild(paragraph);
-
-                console.log(response);
-                contentDiv.appendChild(paragraph);
             }
         }
 
-        xhr.send(null);
+        xhr.send(null);    
+
+        console.log(countryNames.length);
 
     }
+
+    if(countryNames.length > 0) {
+        console.log(countryNames);
+        let countryArraySize = countryNames.length;
+        let countryArrayOutput = document.createElement("h3");
+        countryArrayOutput.innerHTML = countryArraySize;
+        contentDiv.appendChild(countryArrayOutput);
+    }
+
 
 }
 
